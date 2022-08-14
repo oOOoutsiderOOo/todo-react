4fe638549aca83a70a756b3778b4f63f59a07d79
Nicolás Ciancaglini <trancesoup@gmail.com>
2022-08-14 16:13:42 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { nanoid } from "nanoid";
+import { FC, MutableRefObject, useEffect, useRef, useState } from "react";
+import TodoList from "./components/TodoList";
+import useWindowDimensions from "./hooks/useWindowDimensions";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+let initTodos: TodoListType = [{ id: "1", content: "Create my first todo", completed: false }];
+
+export type Todo = {
+    id: string;
+    content: string;
+    completed: boolean;
+};
+export type TodoListType = Todo[];
+
+const App: FC = () => {
+    const [todoList, setTodoList] = useState<TodoListType>([]);
+    const [newTodo, setNewTodo] = useState("");
+    const [fetching, setFetching] = useState(true);
+    const [theme, setTheme] = useState("");
+    const { height, width } = useWindowDimensions();
+
+    useEffect(() => {
+        const initFetch = async () => {
+            !localStorage.getItem("todos") && localStorage.setItem("todos", JSON.stringify(initTodos));
+            !localStorage.getItem("theme") && localStorage.setItem("theme", "dark");
+            let response: TodoListType = JSON.parse(localStorage.getItem("todos") as string);
+            let fetchedTheme = localStorage.getItem("theme") as string;
+            setTodoList(response);
+            setTheme(fetchedTheme);
+        };
+
+        setFetching(true);
+        initFetch();
+        setFetching(false);
+    }, []);
+
+    useEffect(() => {
+        fetching === false && localStorage.setItem("todos", JSON.stringify(todoList));
+    }, [todoList]);
+
+    useEffect(() => {
+        fetching === false && localStorage.setItem("theme", theme);
+    }, [theme]);
+
+    return (
+        <div className={theme !== "" ? (theme === "dark" ? "" : "light") : undefined}>
+            <div className="background">
+                {width > 650 && <img src={theme === "light" ? "/images/bg-desktop-light.jpg" : "/images/bg-desktop-dark.jpg"} alt="" />}
+                {width <= 650 && <img src={theme === "light" ? "/images/bg-mobile-light.jpg" : "/images/bg-mobile-dark.jpg"} alt="" />}
+            </div>
+            <header className="header">
+                <h1 className="title">TODO</h1>
+                <button
+                    className="theme-toggle"
+                    onClick={() => {
+                        theme === "dark" ? setTheme("light") : setTheme("dark");
+                    }}>
+                    <img src={theme === "dark" ? "/images/icon-sun.svg" : "/images/icon-moon.svg"} alt="" />
+                </button>
+            </header>
+            <div className="app-wrapper">
+                <div className="new-todo-row">
+                    <input
+                        className="new-todo-input"
+                        type="text"
+                        value={newTodo}
+                        placeholder="Create a new todo..."
+                        onChange={e => setNewTodo(e.target.value)}
+                        onKeyDown={e => {
+                            if (e.key === "Enter") {
+                                if (newTodo !== "") {
+                                    setTodoList([...todoList, { id: nanoid(10), content: newTodo, completed: false }]);
+                                    setNewTodo("");
+                                }
+                            }
+                        }}
+                    />
+                </div>
+                <TodoList todoList={todoList} setTodoList={setTodoList} />
+            </div>
+            <div className="message">Drag and drop to reorder list</div>
+        </div>
+    );
+};
 
 export default App;
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,82 @@
+import { Dispatch, SetStateAction, useState, useRef, MutableRefObject } from "react";
+import type { TodoListType } from "../App";
+import { Reorder } from "framer-motion";
+import useWindowDimensions from "../hooks/useWindowDimensions";
+
+const TodoList = (props: { todoList: TodoListType; setTodoList: Dispatch<SetStateAction<TodoListType>> }) => {
+    const [filter, setFilter] = useState<"all" | true | false>("all");
+    const [hover, setHover] = useState(-1);
+    const { height, width } = useWindowDimensions();
+
+    return (
+        <>
+            <Reorder.Group className="todo-list" axis="y" values={props.todoList} onReorder={props.setTodoList}>
+                {props.todoList.map(
+                    (item, index) =>
+                        (filter === "all" || filter === item.completed) && (
+                            <Reorder.Item
+                                className="todo-row"
+                                key={item.id}
+                                value={item}
+                                onHoverStart={() => setHover(index)}
+                                onHoverEnd={() => setHover(-1)}>
+                                <div
+                                    className={item.completed ? "complete-check completed-todo" : "complete-check"}
+                                    onClick={() => {
+                                        let updatedTodo = { id: item.id, content: item.content, completed: item.completed === true ? false : true };
+                                        let updatedList = props.todoList.map(todo => todo);
+                                        updatedList.splice(index, 1, updatedTodo);
+                                        props.setTodoList(updatedList);
+                                    }}>
+                                    {item.completed && <img src="/images/icon-check.svg" alt="check completed" />}
+                                </div>
+                                <div className={item.completed === true ? "todo completed-content" : "todo"}>{item.content}</div>
+                                <div
+                                    className="delete"
+                                    onClick={() => props.setTodoList(props.todoList.filter(todo => props.todoList.indexOf(todo) !== index))}>
+                                    {(hover === index || width < 1250) && <img src="/images/icon-cross.svg" alt="delete" />}
+                                </div>
+                            </Reorder.Item>
+                        )
+                )}
+                <div className="actions-row">
+                    <div className="left">{props.todoList.length + " items left"}</div>
+                    <div className="filter">
+                        <button className={filter === "all" ? "all filter-selected" : "all"} onClick={() => setFilter("all")}>
+                            All
+                        </button>
+                        <button className={filter === false ? "active filter-selected" : "active"} onClick={() => setFilter(false)}>
+                            Active
+                        </button>
+                        <button className={filter === true ? "completed filter-selected" : "completed"} onClick={() => setFilter(true)}>
+                            Completed
+                        </button>
+                    </div>
+
+                    <button
+                        className="clear"
+                        onClick={() => {
+                            props.setTodoList(props.todoList.filter(todo => todo.completed !== true));
+                        }}>
+                        Clear Completed
+                    </button>
+                </div>
+            </Reorder.Group>
+            {
+                <div className="filter-mobile">
+                    <button className={filter === "all" ? "all filter-selected" : "all"} onClick={() => setFilter("all")}>
+                        All
+                    </button>
+                    <button className={filter === false ? "active filter-selected" : "active"} onClick={() => setFilter(false)}>
+                        Active
+                    </button>
+                    <button className={filter === true ? "completed filter-selected" : "completed"} onClick={() => setFilter(true)}>
+                        Completed
+                    </button>
+                </div>
+            }
+        </>
+    );
+};
+
+export default TodoList;
